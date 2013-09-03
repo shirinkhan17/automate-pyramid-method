51b8a6d19b96bc5a5af125f902adca2c0b04c8be
Emily Chen <emchennyc@gmail.com>
2013-09-03 08:27:53 -0400

    Added data directory; link README to latent var implementation Added data directory; link README to latent var implementation
    Added headers to python scripts
    
    Commiting in order to rebase
    
    Move python scripts to new directory

--- README.md
+++ README.md
@@ -5,24 +5,28 @@ This study was presented as a [short paper](http://aclweb.org/anthology/P/P13/P1
 
 The pyramid method of summarization evaluation [(Passonneau, 2004)](http://acl.ldc.upenn.edu/hlt-naacl2004/main/pdf/91_Paper.pdf) uses the distribution of content over a pool of human-generated summaries to identify summarization content units (SCUs) that are assigned weights based on their frequency in this corpus of model summaries.
 
-We extended the dynamic programming approach used in [(Harnly, 2005)](http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.59.8121) to automate summarization evaluation with the pyramid method. To test another method of representing the semantic profile of content units, we incorporated a latent variable model [(Guo and Diab, 2012)](https://www.aclweb.org/anthology-new/P/P12/P12-1091v2.pdf) in the dynamic programming algorithm and found excellent performance for matching the absolute scores of individual summaries and improved performance for identifying the content units included in each summary.
+We extended the dynamic programming approach used in [(Harnly, 2005)](http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.59.8121) to automate the scoring procedure for summarization evaluation with the pyramid method. Two additional semantic similarity methods were compared with the one used in (Harnly, 2005), including a latent semantic model, Weighted Matrix Factorization [(WMF; Guo and Diab, 2012)](https://www.aclweb.org/anthology-new/P/P12/P12-1091v2.pdf). WMF yielded excellent performance in reproducing the absolute scores of individual summaries from manual scoring, and very promising results in identifying the content units included in each summary.
 
-These scripts are the key components of our implementation of the work described in the paper.
 
 Data
 ----
 
-5 reference summaries were used to create a pyramid by human annotation and 20 target summaries were evaluated by the pyramid method. We compared results for human and automated evaluation systems.
+5 reference summaries were used to create a pyramid by human annotation and 20 target summaries were evaluated by the pyramid method. We compared results for human and automated evaluation systems. The summaries, pyramid and data used to train the WMF latent variable model are available in this repository.
 
 
-Files
-------
+Code
+----
+These scripts are the key components of our implementation of the work described in the paper. This code is representative of the process, but due to the short term scope of the project, it is not possible to produce complete scripts that run everything end to end.
 
 * determine_thresholds.py outputs 5 threshold values to set as the minimum semantic similarity score that the algorithm will use to classify a match between a string in the target summary being evaluated and a SCU in the pyramid. These 5 values are the semantic similarity scores of strings in the target summaries that have been annotated by a human as semantically equivalent to an SCU in the pyramid, that have inverse cumulative distribution function values of 0.05, 0.10, 0.15, 0.20, 0.25. This script uses a gaussian kernel density estimator to estimate a continuous distribution of similarity scores and plots the distribution.
 
 * dynamic_programming.py runs the dynamic programming algorithm to return the sum of the SCU weights for each of the target summaries.
 
 * get_score_stats.py computes Pearson's correlation of the absolute score for individual summaries, Spearman's rank-order correlation of the ranking of the summaries, Kendall's tau (another metric for summary rank-order correlation) and Jaccard similarity of retrieved content unit sets. This script generates a plot to show the performance of 60 different systems for each of these metrics, displaying a system's performance with a 95% confidence interval.
+
+The WMF implementation used to find latent topics of short text data can be found here: [http://www.cs.columbia.edu/~weiwei/code.html](http://www.cs.columbia.edu/~weiwei/code.html)
+
+
 
 Contact
 -------
---
+++ scripts/dynamic_programming.py
@@ -0,0 +1,104 @@
+## Date: 09-01-2013
+## Author: Emily Chen
+## Usage: python dynamic_programming.py [sim_metric] [combine] [threshold]
+##  sim_metric:
+##      Description- The method used to assign a similarity score to a SCU component and a candidate substring
+##      Format- One of the following: "uni", "ro", "cos"
+##  combine:
+##      Description- The method used to select one of the similarity scores assigned to a candidate substring
+##                  and the components of a SCU
+##      Format- One of the following: "min", "mean", "max"
+##  threshold:
+##      Description- The similarity score above which a candidate substring is considered to match a SCU
+##      Format- A percentage expressed as a decimal rounded to the hundredth position
+
+import argparse
+import pymongo
+import cPickle as pickle
+import sys
+
+from data_storage import Summary
+from joblib import Parallel,delayed
+
+def memo(f):
+    # Memoize function f
+    table = {}
+    def fmemo(*args): 
+        if args not in table:
+            table[args] = f(*args)
+        return table[args]
+    fmemo.memo = table
+    return fmemo
+    
+def get_spanning_set(summary_id, sim_metric, combine, threshold):
+    sentences = pickle.loads(writingsamples.find_one({'id':summary_id})['summary']).sents
+    num_sentences = len(sentences)
+    contributors = []
+    for sent_num in xrange(num_sentences):
+        contributors.append(get_sent_spanning_set(summary_id, sim_metric, sent_num+1, sentences[sent_num], combine, threshold))
+    return contributors
+
+def get_sent_spanning_set(summary_id, sim_metric, sent_num, sentence, combine, threshold):
+    span_start = 0
+    span_end = len(sentence) - 1
+    return f(summary_id,sim_metric,combine,threshold,sent_num,span_start,span_end)
+
+@memo    
+def f(sample,sim_metric,combine,threshold,sent_num,span_start,span_end):
+    if span_start > span_end:
+        return []
+    else:
+        spansets = ((score(sample, sim_metric, combine, threshold, sent_num, span_start, i) + f(sample, sim_metric, combine, threshold, sent_num, i+1, span_end)) for i in xrange(span_start, span_end+1))
+        return max(spansets, key=get_summary_score)
+
+def score(sample, sim_metric, combine, threshold, sent_num, start, end):
+    similarity_metric_params = {"uni": {"collection": db.unigram_5thresholds, 
+                                "db_search_key": "uni_overlap_match"}, 
+                                "ro":, {"collection": db.ro_5thresholds, 
+                                "db_search_key": "ro_match"}, 
+                                "cos": {"collection": db.cos_5thresholds, 
+                                "db_search_key": "cos_match"}}
+    coll = similarity_metric_params[sim_metric]["collection"]
+    db_search_key = similarity_metric_params[sim_metric]["db_search_key"]
+    assert len([i for i in coll.find({"sample":sample, "candidate.start":start, "candidate.end":end, "combine":combine, "threshold":threshold, "sent_num":sent_num})])==1, "mongodb did not return a unique value for candidate query"
+    return [(start, end) + tuple(coll.find_one({"sample":sample, "candidate.start":start, "candidate.end":end, "combine":combine, "threshold":threshold, "sent_num":sent_num})[db_search_key]
+
+def get_summary_score(spanset):
+    scus = []
+    score = 0
+    try: 
+        for sent in spanset:
+            for start, end, match, scu, weight in sent:
+                if scu not in scus: score += weight
+                scus.append(scu)
+    except TypeError:
+       for start, end, match, scu, weight in spanset:
+            if scu not in scus: score += weight
+            scus.append(scu)
+    return score
+
+def store_summ_score(summ_id, sim_metric, combine, threshold, spanset):
+    summary_score = get_summary_score(spanset)
+    summaryscores.save({'id':summ_id, 'spanset':spanset, sim_metric:{'combine':combine, 'threshold':threshold, 'score':summary_score}})
+
+def score_samples(chunk, sim_metric, combine, threshold):
+    for summ_id in chunk:
+        spanset = get_spanning_set(summ_id, sim_metric, combine, threshold)
+        store_summ_score(summ_id, sim_metric, combine ,threshold, spanset)
+
+def chunks(list, chunksize):
+    for i in xrange(0, len(list), chunksize):
+        yield list[i:i+chunksize]
+        
+if __name__ == '__main__':
+    c = pymongo.Connection()
+    db = c.tc_storage
+    writingsamples = db.writingsamples
+    summaryscores = db.summaryscores
+    parser = argparse.ArgumentParser()
+    parser.add_argument("metric", help="The method used to compute a similarity score for two substrings")
+    parser.add_argument("combine", help="The function used to select one similarity score from the set generated for a single SCU. Has one of the following values: [min, mean, max]")
+    parser.add_argument("threshold", help="The similarity score above which two substrings are said to be a match", type=float)
+    args = parser.parse_args() 
+    list_of_ids = [i['id'] for i in writingsamples.find()]
+    Parallel(n_jobs=2, verbose=1)(delayed(score_samples)(args.metric, args.combine, args.threshold) for chunk in chunks(list_of_ids, 5))
